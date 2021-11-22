3b915e23b5d04f4f54802a601115a33686e05337
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-22 22:20:55 +0100

    Create models file and model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_city = models.CharField(max_length=64, blank=True)
+    
+    class Meta:
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return self.user.username

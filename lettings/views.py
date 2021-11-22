8795ea437f494e2dd5aa72b39f709e979b1521d7
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-22 23:06:10 +0100

    Modif def letting_index in def index

--- lettings/views.py
+++ lettings/views.py
@@ -8,10 +8,10 @@ from .models import Letting
 # bibendum id arcu.
 # Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
 # cubilia curae; Cras eget scelerisque
-def lettings_index(request):
+def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'lettings_index.html', context)
+    return render(request, 'lettings/index.html', context)
 
 
 # Cras ultricies dignissim purus, vitae hendrerit ex varius non.
@@ -33,4 +33,4 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, 'lettings/letting.html', context)

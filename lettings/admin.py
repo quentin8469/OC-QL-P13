18bddab1cb4e2a7c4bebd06f8a42d98db5ac747b
Quentin Laparra <quentin.laparra@gmail.com>
2021-11-22 23:11:20 +0100

    register models in admin file

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Address
+from .models import Letting
+
 
 # Register your models here.
+admin.site.register(Address)
+admin.site.register(Letting)

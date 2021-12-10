c58e87934a3baa60994812d3bb044eaf90dda181
Quentin Laparra <quentin.laparra@gmail.com>
2021-12-10 14:54:43 +0100

    modif def setup test for the views*

--- profiles/tests.py
+++ profiles/tests.py
@@ -2,16 +2,17 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-from .models import Profile
+from profiles.models import Profile
 
 # Create your tests here.
 class TestsProfiles(TestCase):
     """"""
-    def setUp(self):
+    @classmethod
+    def setUp(cls):
         user = User.objects.create(username='testuser', password='u1234')
-        print(str(user))
+        #print(str(user))
         profile = Profile.objects.create(user=user , favorite_city='testLyon')
-        print(profile)
+        #print(profile)
     
     def test_profiles_index(self):
         url = reverse('profiles:index')

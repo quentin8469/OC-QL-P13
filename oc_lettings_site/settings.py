ad5a9440f9248a509640adca22b62954ae07727f
Quentin Laparra <quentin.laparra@gmail.com>
2021-12-14 22:36:33 +0100

    Maj file

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret! '&}o]$gL@{7^I-[i5ovJEuRy\t'
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY','fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
